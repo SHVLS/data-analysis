4cd60e06717ba883a80490fbc09040dbd7d9f814
Shevchenko Lesya <L.shevchenko300@gmail.com>
2025-01-08 21:23:58 +0300

    change

--- Case-1/datamart_query.sql
+++ Case-1/datamart_query.sql
@@ -6,7 +6,7 @@ select user_id,
 	recency, 	
 	frequency,	
 	monetary_value 
-	from  analysis.tmp_rfm_frequency 
+from  analysis.tmp_rfm_frequency 
 	join analysis.tmp_rfm_recency  using  (user_id)
 	join analysis.tmp_rfm_monetary_value using (user_id);
 	
--- Case-1/orders_view.sql
+++ Case-1/orders_view.sql
@@ -4,4 +4,12 @@
 Дата проставления статуса заказа и сам статус хранятся в новой таблице production.OrderStatusLog.*/
 
 
-create or REPLACE view analysis.orders as (select  o.order_id,  orl.status_id as status , o.user_id, o.order_ts, o.cost from production.orders o  join production.orderstatuslog orl on o.order_id = orl.order_id and o.order_ts = orl.dttm );
+create or REPLACE view analysis.orders as (
+    select  o.order_id,  
+        orl.status_id as status , 
+        o.user_id, 
+        o.order_ts, 
+        o.cost 
+    from production.orders o  
+        join production.orderstatuslog orl on o.order_id = orl.order_id and o.order_ts = orl.dttm 
+    );
